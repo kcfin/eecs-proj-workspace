b44ff906e0ac03f8dc12b27e847195944280d8da
kcfin <kcfin@cse1695m02.engin.umich.edu>
2014-11-18 11:01:18 -0500

    adding p4

---
+++ EECS281P4/EECS281P4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EECS281P4
+//
+//  Created by kcfin on 11/18/14.
+//  Copyright (c) 2014 eecs281. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
