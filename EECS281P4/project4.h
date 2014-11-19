f1525d93c438715613028d66a15018a99152e69b
Katelyn Findlay <kcfin@caen-vnc20.engin.umich.edu>
2014-11-19 10:13:56 -0500

    adding project 4

---
+++ EECS281P4/project4.h
@@ -0,0 +1,14 @@
+//
+//  project4.h
+//  EECS281P4
+//
+//  Created by kcfin on 11/19/14.
+//  Copyright (c) 2014 eecs281. All rights reserved.
+//
+
+#ifndef __EECS281P4__project4__
+#define __EECS281P4__project4__
+
+#include <iostream>
+
+#endif /* defined(__EECS281P4__project4__) */
---
+++ project4.cpp
@@ -0,0 +1,9 @@
+//
+//  project4.cpp
+//  EECS281P4
+//
+//  Created by kcfin on 11/19/14.
+//  Copyright (c) 2014 eecs281. All rights reserved.
+//
+
+#include "project4.h"
